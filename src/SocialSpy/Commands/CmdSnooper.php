e00597c092c30ace53ad3a13cc69aefa95ef3217
okstate3401 <25288311+okstate3401@users.noreply.github.com>
2017-08-18 12:49:26 -0500

    Update CmdSnooper.php

--- src/SocialSpy/Commands/CmdSnooper.php
+++ src/SocialSpy/Commands/CmdSnooper.php
@@ -1,1 +1,17 @@
+<?php
+
+namespace SocialSpy\Commands
+  
+use SocialSpy\SocialSpy
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\command\CommandExecutor;
+  
+class SnoopCommand implements CommandExecutor{
+  
+  
+  
+}
 
