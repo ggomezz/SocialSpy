d6f660636a76d43bc15fefb767913c06e49bbb3e
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2017-08-18 16:50:25 -0500

    Inventory See

--- src/SocialSpy/SocialSpy.php
+++ src/SocialSpy/SocialSpy.php
@@ -7,16 +7,41 @@ use pocketmine\utils\Config;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
+use pocketmine\command\PluginCommand;
 
-class Main extends PluginBase{
+use SocialSpy\Command\ViewInv;
+use SocialSpy\Session\ViewInv as ViewSession;
+
+
+class SocialSpy extends PluginBase{
   
-  public function onEnable(){
+  	public function onEnable(){
        
-    }
+        }
     
-  public function onDisable(){
+  	public function onDisable(){
         
-    }  
-}
+        }  
 	
+	public function registerCommands() {
+		$cmd = new PluginCommand("viewinv", $this);
+                $cmd->setDescription("View a players inventory");
+                $cmd->setExecutor(new ViewInv($this));
+                $this->getServer()->getCommandMap()->register("ic", $cmd);
+        }
 
+  	public function viewInventory(Player $player, Player $target) {
+          	$this->viewing[$player->getName()] = new ViewSession($player, $target, true);
+        }
+        
+  	public function isViewing($name) {
+          	return isset($this->viewing[$name]) and $this->viewing[$name] instanceof ViewSession;
+        }
+        
+  	public function stopViewing($name) {
+          	if(!$this->isViewing($name)) return;
+          	$this->viewing[$name]->end();
+          	unset($this->viewing[$name]);
+    	}
+
+}
