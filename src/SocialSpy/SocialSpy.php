6b631f8e4533c37e3b8742b6847937ac9d2f3d15
okstate3401 <25288311+okstate3401@users.noreply.github.com>
2017-08-18 12:03:10 -0500

    Update SocialSpy.php

--- src/SocialSpy/SocialSpy.php
+++ src/SocialSpy/SocialSpy.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace SocialSpy;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+       
+    }
+    
+    public function onDisable(){
+        
+    }  
+}
