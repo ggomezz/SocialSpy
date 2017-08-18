05c67fb181c2121b8742430d374842dc1d5cee27
okstate3401 <25288311+okstate3401@users.noreply.github.com>
2017-08-18 12:05:21 -0500

    Update EventListener.php

--- src/SocialSpy/EventListener.php
+++ src/SocialSpy/EventListener.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace SocialSpy;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use SocialSpy\SocialSpy;
+
+class Main extends PluginBase implements Listener{
+  
+  
+  
+  
+  
+}
