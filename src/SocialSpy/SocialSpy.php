00ddd5e44d94ca767c8e931f63ef6e1c196ff118
okstate3401 <25288311+okstate3401@users.noreply.github.com>
2017-08-18 23:06:14 -0500

    OnEnable(), onDisable(), add a few public functions

--- src/SocialSpy/SocialSpy.php
+++ src/SocialSpy/SocialSpy.php
@@ -15,13 +15,78 @@ use SocialSpy\Session\ViewInv as ViewSession;
 
 class SocialSpy extends PluginBase{
   
+	public $database;
+	
+	public $authorized = array();
+	
   	public function onEnable(){
-       
+       		$this->getLogger()->info('§ePlugin initialization...');
+		
+		@mkdir($this->getDataFolder());
+		
+		if(!is_file($this->getDataFolder(). 'config.yml')){
+			$this->saveResource('config.yml'); 
+		}
+		
+		$this->config = new Config($this->getDataFolder(). 'config.yml', Config::YAML);
+		
+		$databaseType = strtolower($this->config->get('type'));
+		switch($databaseType){
+			case 'mysql':
+			case 'mysqli':
+				$this->database = new Database\MySQLDatabase($this);
+				break;
+				
+			case 'yaml':
+			case 'yml':
+				$this->database = new Database\YAMLDatabase($this);
+				break;
+			
+			case 'json':
+				$this->database = new Database\JSONDatabase($this);
+				break;
+				
+			case 'sqlite':
+			case 'sqlite3':
+				$this->database = new Database\SQLiteDatabase($this);
+				break;
+				
+			default:
+				$this->getLogger()->warning($this->lang->getMessage('db_wrong', ['{type}'], [$databaseType]));
+				$this->database = new Database\SQLiteDatabase($this);
+		}
+		
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		
+		$this->getCommand("snoop")->setExecutor(new Commands\CmdSnooper($this));
         }
     
   	public function onDisable(){
-        
+        	$this->database->close();
         }  
+	
+	public function getDatabase(){
+		return $this->database;
+	}
+	
+	public function isAuthorized(Player $player){
+		return isset($this->authorized[strtolower($player->getName())]);
+	}
+	
+	public function authorize(Player $player){
+		$nick = strtolower($player->getName());
+		$this->setVisible($player);
+		
+		$this->authorized[$nick] = true;
+		$this->database->authorizePlayer($player);
+		return;
+	}
+	
+	public function deauthorize(Player $player){
+		$this->setInvisible($player);
+		unset($this->authorized[strtolower($player->getName())]);
+		return;
+	}
 	
 	public function registerCommands() {
 		$cmd = new PluginCommand("viewinv", $this);
