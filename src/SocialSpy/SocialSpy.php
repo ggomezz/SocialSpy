64965d5fa60b16814fd063af1f5adebc60e70e3a
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2017-08-19 20:03:49 -0500

    Update SocialSpy.php

--- src/SocialSpy/SocialSpy.php
+++ src/SocialSpy/SocialSpy.php
@@ -14,7 +14,7 @@ use SocialSpy\Command\ViewInv;
 use SocialSpy\Session\ViewInv as ViewSession;
 
 
-class SocialSpy extends PluginBase{
+class socialspy extends PluginBase{
 	
 	public $authorized = array();
 	
