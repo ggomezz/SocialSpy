590ac278d0a852a8224cece935661c7713c0ef13
okstate3401 <25288311+okstate3401@users.noreply.github.com>
2017-08-19 19:40:32 -0500

    Update CmdSnooper.php

--- src/SocialSpy/Commands/CmdSnooper.php
+++ src/SocialSpy/Commands/CmdSnooper.php
@@ -10,8 +10,15 @@ use pocketmine\Player;
 use pocketmine\command\CommandExecutor;
   
 class SnoopCommand implements CommandExecutor{
-  
-  
+    public function __construct(SocialSpy $plugin){
+      $this->plugin = $plugin;
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+      if($this->plugin->isAuthorized($sender)){
+
+        return true;
+      }  
   
 }
 
