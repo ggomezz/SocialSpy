9b906f0d40934331e1b14df04a26a751917a907f
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2019-03-17 17:03:34 -0500

    Update CmdSnooper.php

--- src/SocialSpy/Commands/CmdSnooper.php
+++ src/SocialSpy/Commands/CmdSnooper.php
@@ -1,29 +1,42 @@
 <?php
 
-namespace SocialSpy\Commands;
-  
-use SocialSpy\SocialSpy;
+namespace cmdsnooper;
+
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\Player;
-use pocketmine\command\CommandExecutor;
-  
-class CmdSnooper implements CommandExecutor{
-    public function __construct(SocialSpy $plugin){
-      $this->plugin = $plugin;
-    }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-      if(!$this->plugin->isAuthorized($sender)){
-        $this->plugin->authorize($sender);
-        return true;
-      } 
-      else{
-        $this->plugin->deauthorize($sender);
-        return true;
-      }
-    }
-  
-}
-
+class CmdSnooper extends PluginBase {
+	public $snoopers = [];
+	
+	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		$this->getLogger()->info("Enabled! Ready to snoop >:D");
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+	  	"Console.Logger" => "true",
+  		));
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{				
+		if(strtolower($cmd->getName()) == "snoop") {
+		 	if($sender instanceof Player) {
+				if($sender->hasPermission("snoop.command")) {
+					if(!isset($this->snoopers[$sender->getName()])) {
+						$sender->sendMessage("§8Snoop> §eYou have entered snoop mode");
+						$this->snoopers[$sender->getName()] = $sender;
+						return true;
+					} else {
+						$sender->sendMessage("§8Snoop> §eYou have left snoop mode");
+						unset($this->snoopers[$sender->getName()]);
+						return true;
+					}
+				}
+			}
+		}
+		$this->getLogger()->info("Command '/snoop' must be run as a player");
+		return false;
+	}
+ }
